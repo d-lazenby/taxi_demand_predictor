bd2923bfc7ba4801024da11f395429fb32e4f657
Doug <douglazenby@gmail.com>
2024-10-01 13:12:17 +0100

    typo: fixing typo

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ load_dotenv(PARENT_DIR / '.env')
 
 HOPSWORKS_PROJECT_NAME = 'taxi_demand_DL'
 try: 
-    HOPSWORKS_API_KEY = os.environ('HOPSWORKS_API_KEY')
+    HOPSWORKS_API_KEY = os.environ['HOPSWORKS_API_KEY']
 except:
     raise Exception('Create a .env file on the project root with the HOPSWORKS_API_KEY')
 
@@ -17,3 +17,6 @@ FEATURE_VIEW_NAME = 'time_series_hourly_feature_view'
 FEATURE_VIEW_VERSION = 1
 MODEL_NAME = "taxi_demand_predictor_next_hour"
 MODEL_VERSION = 1
+
+# number of historical values our model needs to generate predictions
+N_FEATURES = 24 * 28
